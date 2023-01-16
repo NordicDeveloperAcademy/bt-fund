9afeecfdf76cda661586dfd51dacd61153a51faf
Ali Aljaani <academy@nordicsemi.no>
2023-01-16 14:05:36 +0100

    Lesson 2- Exercise 1 Solution Steps

--- lesson2/blefund_less2_exer2_solution/src/main.c
+++ lesson2/blefund_less2_exer2_solution/src/main.c
@@ -13,7 +13,14 @@
 #include <zephyr/bluetooth/uuid.h>
 
 #include <dk_buttons_and_leds.h>
-
+#define COMPANY_CODE            0x0059
+typedef struct adv_mfg_data {
+	uint16_t company_code;	    /* Company Identifier Code. */
+	uint16_t number_press;   
+} adv_mfg_data_type;
+#define USER_BUTTON             DK_BTN1_MSK
+static adv_mfg_data_type adv_mfg_data = {COMPANY_CODE,0x00};
+static unsigned char url_data[] ={0x17,'/','/','a','c','a','d','e','m','y','.','n','o','r','d','i','c','s','e','m','i','.','c','o','m'};
 LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
 
 #define DEVICE_NAME             CONFIG_BT_DEVICE_NAME
@@ -21,27 +28,40 @@ LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
 
 #define RUN_STATUS_LED          DK_LED1
 #define RUN_LED_BLINK_INTERVAL  1000
-
-/* STEP 4.1.1 - Declare the advertising packet */
+/* STEP 4.1 - Declare the advertising packet */
 static const struct bt_data ad[] = {
-	/* STEP 4.1.2 - Set the advertising flags */
+	/* STEP 4.1.1 - Set the advertising flags */
 	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
-	/* STEP 4.1.3 - Set the advertising packet data  */
+	/* STEP 4.1.2 - Set the advertising data  */
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+	BT_DATA(BT_DATA_MANUFACTURER_DATA,(unsigned char *)&adv_mfg_data, sizeof(adv_mfg_data)),
 
 };
 
-/* STEP 4.2.2 - Declare the URL data to include in the scan response */
-static unsigned char url_data[] ={0x17,'/','/','a','c','a','d','e','m','y','.',
-                                 'n','o','r','d','i','c','s','e','m','i','.',
-                                 'c','o','m'};
-
-/* STEP 4.2.1 - Declare the scan response packet */
 static const struct bt_data sd[] = {
-    /* 4.2.3 Include the URL data in the scan response packet*/
 	BT_DATA(BT_DATA_URI, url_data,sizeof(url_data)),
 };
 
+static void button_changed(uint32_t button_state, uint32_t has_changed)
+{
+	if (has_changed & button_state & USER_BUTTON) {
+	adv_mfg_data.number_press += 1;
+	bt_le_adv_update_data(ad, ARRAY_SIZE(ad),
+			      sd, ARRAY_SIZE(sd));
+	}
+}
+
+static int init_button(void)
+{
+	int err;
+
+	err = dk_buttons_init(button_changed);
+	if (err) {
+		printk("Cannot init buttons (err: %d)\n", err);
+	}
+
+	return err;
+}
 void main(void)
 {
 	int blink_status = 0;
@@ -54,7 +74,11 @@ void main(void)
 		LOG_ERR("LEDs init failed (err %d)\n", err);
 		return;
 	}
-
+	err = init_button();
+	if (err) {
+		printk("Button init failed (err %d)\n", err);
+		return;
+	}
 	err = bt_enable(NULL);
 	if (err) {
 		LOG_ERR("Bluetooth init failed (err %d)\n", err);
@@ -62,8 +86,7 @@ void main(void)
 	}
 
 	LOG_INF("Bluetooth initialized\n");
-	
-	/* STEP 5 - Start advertising */
+
 	err = bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad),
 			      sd, ARRAY_SIZE(sd));
 	if (err) {
