3590e02518f97fef9208bca12c64875bdef27999
Sollie <heidi-irene.sollie@nordicsemi.no>
2023-03-10 12:18:27 +0100

    Added exercise for lesson 6

--- lesson6/blefund_less6_exer2/src/main.c
+++ lesson6/blefund_less6_exer2/src/main.c
@@ -9,82 +9,243 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/gap.h>
-/*STEP 3.2.1 - Include the header file of the UUID helper macros and definitions */
+#include <zephyr/bluetooth/gatt.h>
 #include <zephyr/bluetooth/uuid.h>
-/*STEP 4.1 - Include the header file for managing Bluetooth LE addresses */
 #include <zephyr/bluetooth/addr.h>
+#include <zephyr/bluetooth/conn.h>
+#include <bluetooth/services/lbs.h>
+
 
 #include <dk_buttons_and_leds.h>
 
 
-/* STEP 5.1 - Create the advertising parameter for connectable advertising */
 static struct bt_le_adv_param *adv_param = BT_LE_ADV_PARAM((BT_LE_ADV_OPT_CONNECTABLE|BT_LE_ADV_OPT_USE_IDENTITY), /* Connectable advertising and use identity address */
-                800, /*Min Advertising Interval 500ms (800*0.625ms) */
-                801, /*Max Advertising Interval 500.625ms (801*0.625ms)*/
+                BT_GAP_ADV_FAST_INT_MIN_1, /* 0x30 units, 48 units, 30ms */
+                BT_GAP_ADV_FAST_INT_MAX_1, /* 0x60 units, 96 units, 60ms */
                 NULL); /* Set to NULL for undirected advertising*/
 
 
-LOG_MODULE_REGISTER(Lesson2_Exercise3, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(Lesson3_Exercise2, LOG_LEVEL_INF);
+struct bt_conn *my_conn = NULL;
+
+/* STEP 11 - Create variable that holds callback for MTU negotiation */
+static struct bt_gatt_exchange_params exchange_params;
+
+/* STEP 13.4 - forward declaration of exchange_func(): */
+static void exchange_func(struct bt_conn *conn, uint8_t att_err, struct bt_gatt_exchange_params *params);
+
 
 #define DEVICE_NAME             CONFIG_BT_DEVICE_NAME
 #define DEVICE_NAME_LEN         (sizeof(DEVICE_NAME) - 1)
 
+#define USER_BUTTON             DK_BTN1_MSK
 #define RUN_STATUS_LED          DK_LED1
+#define CONNECTION_STATUS_LED   DK_LED2
 #define RUN_LED_BLINK_INTERVAL  1000
 
+
+
 static const struct bt_data ad[] = {
-	/* STEP 3.1 - Set the flags and populate the device name in the advertising packet */
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
-
 };
 
 static const struct bt_data sd[] = {
-	/* STEP 3.2.2 - Include the 16-bytes (128-Bits) UUID of the LBS service in the scan response packet */
 	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_128_ENCODE(0x00001523, 0x1212, 0xefde, 0x1523, 0x785feabcd123)),
 };
+
+/* STEP 7 - Update the connection's PHY */
+static void update_phy(struct bt_conn *conn)
+{
+    int err;
+    const struct bt_conn_le_phy_param preferred_phy = {
+        .options = BT_CONN_LE_PHY_OPT_NONE,
+        .pref_rx_phy = BT_GAP_LE_PHY_2M,
+        .pref_tx_phy = BT_GAP_LE_PHY_2M,
+    };
+    err = bt_conn_le_phy_update(conn, &preferred_phy);
+    if (err) {
+        LOG_ERR("bt_conn_le_phy_update() returned %d", err);
+    }
+}
+
+/* STEP 10 - Update the connection data length */
+static void update_data_length(struct bt_conn *conn)
+{
+    int err;
+    struct bt_conn_le_data_len_param my_data_len = {
+        .tx_max_len = BT_GAP_DATA_LEN_MAX,
+        .tx_max_time = BT_GAP_DATA_TIME_MAX,
+    };
+    err = bt_conn_le_data_len_update(my_conn, &my_data_len);
+    if (err) {
+        LOG_ERR("data_len_update failed (err %d)", err);
+    }
+}
+
+/* STEP 11 - Update MTU */
+static void update_mtu(struct bt_conn *conn)
+{
+    int err;
+    exchange_params.func = exchange_func;
+
+    err = bt_gatt_exchange_mtu(conn, &exchange_params);
+    if (err) {
+        LOG_ERR("bt_gatt_exchange_mtu failed (err %d)", err);
+    }
+}
+
+/* Callbacks */
+void on_connected(struct bt_conn *conn, uint8_t err)
+{
+    if (err) {
+        LOG_ERR("Connection error %d", err);
+        return;
+    }
+    LOG_INF("Connected");
+    my_conn = bt_conn_ref(conn);
+    dk_set_led(CONNECTION_STATUS_LED, 1);
+    /* STEP 1 - Fetch connection parameters from the current connection */
+    struct bt_conn_info info;
+    err = bt_conn_get_info(conn, &info);
+    if (err) {
+        LOG_ERR("bt_conn_get_info() returned %d", err);
+        return;
+    }
+    double connection_interval = info.le.interval*1.25; // in ms
+    uint16_t supervision_timeout = info.le.interval*10; // in ms
+    LOG_INF("Connection parameters: interval %.2f ms, latency %d intervals, timeout %d ms", connection_interval, info.le.latency, supervision_timeout);
+
+    /* STEP 13.5 - update all connection parameters */
+    update_phy(my_conn);
+    update_data_length(my_conn);
+    update_mtu(my_conn);
+}
+
+void on_disconnected(struct bt_conn *conn, uint8_t reason)
+{
+    LOG_INF("Disconnected. Reason %d", reason);
+    dk_set_led(CONNECTION_STATUS_LED, 0);
+    bt_conn_unref(my_conn);
+}
+
+/* STEP 4 - Add the callback for connection parameter updates */
+void on_le_param_updated(struct bt_conn *conn, uint16_t interval, uint16_t latency, uint16_t timeout)
+{
+    double connection_interval = interval*1.25;         // in ms
+    uint16_t supervision_timeout = timeout*10;          // in ms
+    LOG_INF("Connection parameters updated: interval %.2f ms, latency %d intervals, timeout %d ms", connection_interval, latency, supervision_timeout);
+}
+
+/* STEP 8 - Write a callback function to inform about updates in the PHY */
+void on_le_phy_updated(struct bt_conn *conn, struct bt_conn_le_phy_info *param)
+{
+    // PHY Updated
+    if (param->tx_phy == BT_CONN_LE_TX_POWER_PHY_1M) {
+        LOG_INF("PHY updated. New PHY: 1M");
+    }
+    else if (param->tx_phy == BT_CONN_LE_TX_POWER_PHY_2M) {
+        LOG_INF("PHY updated. New PHY: 2M");
+    }
+    else if (param->tx_phy == BT_CONN_LE_TX_POWER_PHY_CODED_S8) {
+        LOG_INF("PHY updated. New PHY: Long Range");
+    }
+}
+/* STEP 13.1 - Write a callback function to inform about updates in data length*/
+void on_le_data_len_updated(struct bt_conn *conn, struct bt_conn_le_data_len_info *info)
+{
+    uint16_t tx_len     = info->tx_max_len; 
+    uint16_t tx_time    = info->tx_max_time;
+    uint16_t rx_len     = info->rx_max_len;
+    uint16_t rx_time    = info->rx_max_time;
+    LOG_INF("Data length updated. Length %d/%d bytes, time %d/%d us", tx_len, rx_len, tx_time, rx_time);
+}
+
+struct bt_conn_cb connection_callbacks = {
+    .connected              = on_connected,
+    .disconnected           = on_disconnected,
+    /* STEP 4 - Add the callback for connection parameter updates */
+    .le_param_updated       = on_le_param_updated,
+    /* STEP 8 - Add the phy_updated callback */
+    .le_phy_updated         = on_le_phy_updated,
+    /* STEP 13.2 - Add the callback for data length updates */
+    .le_data_len_updated    = on_le_data_len_updated,
+};
+
+/* STEP 13.3 - Implement callback function for MTU exchange */
+static void exchange_func(struct bt_conn *conn, uint8_t att_err,
+			  struct bt_gatt_exchange_params *params)
+{
+	LOG_INF("MTU exchange %s", att_err == 0 ? "successful" : "failed");
+    if (!att_err) {
+        uint16_t payload_mtu = bt_gatt_get_mtu(conn) - 3;   // 3 bytes used for Attribute headers.
+        LOG_INF("New MTU: %d bytes", payload_mtu);
+    }
+}
+
+
+/* STEP 5 - Send a notification using the Battery Service */
+
+static void button_changed(uint32_t button_state, uint32_t has_changed)
+{
+    int err;
+    if (has_changed & USER_BUTTON) {
+        LOG_INF("Button changed");
+        err = bt_lbs_send_button_state(button_state ? true : false);
+        if (err) {
+            LOG_ERR("Couldn't send notification. err: %d", err);
+        }
+    }
+}
+
+static int init_button(void)
+{
+	int err;
+
+	err = dk_buttons_init(button_changed);
+	if (err) {
+		LOG_INF("Cannot init buttons (err: %d)", err);
+	}
+
+	return err;
+}
 
 void main(void)
 {
 	int blink_status = 0;
 	int err;
 
-	LOG_INF("Starting Lesson 2 - Exercise 1 \n");
+	LOG_INF("Starting Lesson 3 - Exercise 1\n");
 
 	err = dk_leds_init();
 	if (err) {
-		LOG_ERR("LEDs init failed (err %d)\n", err);
+		LOG_ERR("LEDs init failed (err %d)", err);
+		return;
+	}
+	
+	err = init_button();
+	if (err) {
+		LOG_ERR("Button init failed (err %d)", err);
 		return;
 	}
 
-	/* STEP 4.2 - Change the random static address */
-    bt_addr_le_t addr;
-    err = bt_addr_le_from_str("FF:EE:DD:CC:BB:AA", "random", &addr);
-    if (err) {
-        printk("Invalid BT address (err %d)\n", err);
-    }
-
-    err = bt_id_create(&addr, NULL);
-    if (err < 0) {
-        printk("Creating new ID failed (err %d)\n", err);
-    }
 
 	err = bt_enable(NULL);
 	if (err) {
-		LOG_ERR("Bluetooth init failed (err %d)\n", err);
-		return;
-	}
- 
-	LOG_INF("Bluetooth initialized\n");
-	/* STEP 5.2 - Start advertising */
-	err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad),
-			      sd, ARRAY_SIZE(sd));
-	if (err) {
-		LOG_ERR("Advertising failed to start (err %d)\n", err);
+		LOG_ERR("Bluetooth init failed (err %d)", err);
 		return;
 	}
 
-	LOG_INF("Advertising successfully started\n");
+    bt_conn_cb_register(&connection_callbacks);
+	LOG_INF("Bluetooth initialized");
+	err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad),
+			      sd, ARRAY_SIZE(sd));
+	if (err) {
+		LOG_ERR("Advertising failed to start (err %d)", err);
+		return;
+	}
+
+	LOG_INF("Advertising successfully started");
 
 	for (;;) {
 		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
