47ba37ed4851edaced31f5d44c3f7d994b20df03
Jakub Zimnol <j.zimnol@avsystem.com>
2023-08-15 16:15:24 +0200

    Fix typos in comments

--- lesson2/blefund_less2_exer2_solution/src/main.c
+++ lesson2/blefund_less2_exer2_solution/src/main.c
@@ -23,7 +23,7 @@ typedef struct adv_mfg_data {
 
 /* STEP 1 - Create an LE Advertising Parameters variable */
 static struct bt_le_adv_param *adv_param =
-	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_NONE, /* No options spesified */
+	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_NONE, /* No options specified */
 			800, /* Min Advertising Interval 500ms (800*0.625ms) */
 			801, /* Max Advertising Interval 500.625ms (801*0.625ms) */
 			NULL); /* Set to NULL for undirected advertising */
--- lesson6/blefund_less6_exer1/src/main.c
+++ lesson6/blefund_less6_exer1/src/main.c
@@ -20,7 +20,7 @@ typedef struct adv_mfg_data {
 #define USER_BUTTON DK_BTN1_MSK
 
 static struct bt_le_adv_param *adv_param =
-	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_NONE, /* No options spesified */
+	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_NONE, /* No options specified */
 			800, /* Min Advertising Interval 500ms (800*0.625ms) */
 			801, /* Max Advertising Interval 500.625ms (801*0.625ms) */
 			NULL); /* Set to NULL for undirected advertising */
