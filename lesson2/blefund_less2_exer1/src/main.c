550a4f696ae0349ab14942893822708633a3c7de
Natalia Pluta <pluta.natalia.m@gmail.com>
2023-12-10 16:52:07 +0100

    add resolution of ex.1 lesson_2

--- lesson2/blefund_less2_exer1/src/main.c
+++ lesson2/blefund_less2_exer1/src/main.c
@@ -6,7 +6,9 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-/* STEP 3 - Include the header file of the Bluetooth LE stack */
+ /* STEP 3 - Include the header file of the Bluetooth LE stack */
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gap.h>
 
 #include <dk_buttons_and_leds.h>
 
@@ -19,31 +21,60 @@ LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
 #define RUN_LED_BLINK_INTERVAL 1000
 
 /* STEP 4.1.1 - Declare the advertising packet */
+static const struct bt_data ad[] = {
+    /* STEP 4.1.2 - Set the advertising data */
+    BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+    /* STEP 4.1.3 - Set the advertising packet data */
+    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
 
 /* STEP 4.2.2 - Declare the URL data to include in the scan response */
+static unsigned char url_data[] = { 0x17,'/','/','a','c','a','d','e','m','y','.',
+                                 'n','o','r','d','i','c','s','e','m','i','.',
+                                 'c','o','m' };
 
 /* STEP 4.2.1 - Declare the scan response packet */
+static const struct bt_data sd[] = {
+    /* STEP 4.2.3 - Include the URL data in the scan response */
+    BT_DATA(BT_DATA_URI, url_data, sizeof(url_data)),
+};
 
 void main(void)
 {
-	int blink_status = 0;
-	int err;
+    int blink_status = 0;
+    int err;
 
-	LOG_INF("Starting Lesson 2 - Exercise 1 \n");
+    LOG_INF("Starting Lesson 2 - Exercise 1 \n");
 
-	err = dk_leds_init();
-	if (err) {
-		LOG_ERR("LEDs init failed (err %d)\n", err);
-		return;
-	}
-	/* STEP 5 - Enable the Bluetooth LE stack */
+    err = dk_leds_init();
+    if (err)
+    {
+        LOG_ERR("LEDs init failed (err %d)\n", err);
+        return;
+    }
 
-	/* STEP 6 - Start advertising */
+    /* STEP 5 - Enable the Bluetooth LE stack */
+    err = bt_enable(NULL);
+    if (err)
+    {
+        LOG_ERR("Bluetooth init failed (err %d)\n", err);
+        return;
+    }
+    LOG_INF("Bluetooth initialized\n");
 
-	LOG_INF("Advertising successfully started\n");
+    /* STEP 6 - Start advertising */
+    err = bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+    if (err)
+    {
+        LOG_ERR("Advertising failed to start (err %d)\n", err);
+        return;
+    }
 
-	for (;;) {
-		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
-		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
-	}
+    LOG_INF("Advertising successfully started\n");
+
+    for (;;)
+    {
+        dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
+        k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+    }
 }
