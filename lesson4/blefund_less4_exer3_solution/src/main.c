a76cbb10266470eb90d9fb8a13c8d4b4e0b5d060
AliNordic <ali.aljaani@nordicsemi.no>
2024-04-15 15:30:17 +0200

    Update CONTAINER_OF usage Updates macro usage to fix a compilation error

--- lesson4/blefund_less4_exer3_solution/src/main.c
+++ lesson4/blefund_less4_exer3_solution/src/main.c
@@ -103,11 +103,13 @@ static void uart_cb(const struct device *dev, struct uart_event *evt, void *user
 		}
 
 		if (aborted_buf) {
-			buf = CONTAINER_OF(aborted_buf, struct uart_data_t, data);
+			buf = CONTAINER_OF(aborted_buf, struct uart_data_t,
+					   data[0]);
 			aborted_buf = NULL;
 			aborted_len = 0;
 		} else {
-			buf = CONTAINER_OF(evt->data.tx.buf, struct uart_data_t, data);
+			buf = CONTAINER_OF(evt->data.tx.buf, struct uart_data_t,
+					   data[0]);
 		}
 
 		k_free(buf);
@@ -125,7 +127,7 @@ static void uart_cb(const struct device *dev, struct uart_event *evt, void *user
 
 	case UART_RX_RDY:
 		LOG_DBG("UART_RX_RDY");
-		buf = CONTAINER_OF(evt->data.rx.buf, struct uart_data_t, data);
+		buf = CONTAINER_OF(evt->data.rx.buf, struct uart_data_t, data[0]);
 		buf->len += evt->data.rx.len;
 
 		if (disable_req) {
@@ -171,7 +173,8 @@ static void uart_cb(const struct device *dev, struct uart_event *evt, void *user
 
 	case UART_RX_BUF_RELEASED:
 		LOG_DBG("UART_RX_BUF_RELEASED");
-		buf = CONTAINER_OF(evt->data.rx_buf.buf, struct uart_data_t, data);
+		buf = CONTAINER_OF(evt->data.rx_buf.buf, struct uart_data_t,
+				   data[0]);
 
 		if (buf->len > 0) {
 			/* STEP 9.1 -  Push the data received from the UART peripheral into the fifo_uart_rx_data FIFO */
@@ -189,7 +192,8 @@ static void uart_cb(const struct device *dev, struct uart_event *evt, void *user
 		}
 
 		aborted_len += evt->data.tx.len;
-		buf = CONTAINER_OF(aborted_buf, struct uart_data_t, data);
+		buf = CONTAINER_OF((void *)aborted_buf, struct uart_data_t,
+				   data);
 
 		uart_tx(uart, &buf->data[aborted_len], buf->len - aborted_len, SYS_FOREVER_MS);
 
